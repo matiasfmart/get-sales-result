6f05fdb67754243282f6d27c4940f38b1d7744dd
matias.martinez <matias.martinez@C11-L7HU3DSZERB>
2025-08-05 22:23:32 -0300

    update configs

--- get-sales-result/Program.cs
+++ get-sales-result/Program.cs
@@ -1,35 +1,27 @@
-using get_sales_result.Application.Interfaces;
-using get_sales_result.Application.Services;
-using get_sales_result.Infra;
-using get_sales_result.WebApi.Middlewares;
+using get_sales_result.WebApi.Extensions;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddSingleton<InMemorySalesRepository>();
-builder.Services.AddScoped<IVehicleSaleService, VehicleSaleService>();
+builder.Services.AddApplicationServices();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-    c.IncludeXmlComments(xmlPath);
-});
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
+
+builder.Services.AddCustomSwagger();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseMiddleware<ExecutionTimeMiddleware>();
-app.UseMiddleware<GlobalExceptionMiddleware>();
+app.UseCustomMiddlewares();
 
 app.UseHttpsRedirection();
 
---
+++ get-sales-result/WebApi/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,39 @@
+﻿using get_sales_result.Application.Interfaces;
+using get_sales_result.Application.Services;
+using get_sales_result.Infra;
+using get_sales_result.WebApi.Middlewares;
+
+namespace get_sales_result.WebApi.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddSingleton<InMemorySalesRepository>();
+            services.AddScoped<IVehicleSaleService, VehicleSaleService>();
+            return services;
+        }
+
+        public static IServiceCollection AddCustomSwagger(this IServiceCollection services)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(c =>
+            {
+                var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+            return services;
+        }
+    }
+
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseCustomMiddlewares(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ExecutionTimeMiddleware>();
+            app.UseMiddleware<GlobalExceptionMiddleware>();
+            return app;
+        }
+    }
+}
